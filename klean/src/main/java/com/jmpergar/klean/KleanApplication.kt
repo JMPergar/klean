40ad0779d96bafbe70d0f21d34c57f61306032c0
JMPergar <jm.pereira.g@gmail.com>
2017-03-06 18:42:19 +0100

    Add Lynx library

--- klean/src/main/java/com/jmpergar/klean/KleanApplication.kt
+++ klean/src/main/java/com/jmpergar/klean/KleanApplication.kt
@@ -2,12 +2,14 @@ package com.jmpergar.klean
 
 import android.app.Application
 import android.os.StrictMode
+import com.github.pedrovgs.lynx.LynxShakeDetector
 
 abstract class KleanApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         setUpStrictMode()
+        setUpLynx()
     }
 
     private fun setUpStrictMode() {
@@ -22,4 +24,14 @@ abstract class KleanApplication : Application() {
                     .build())
         }
     }
+
+    private fun setUpLynx() {
+        // Is need to add this to main manifest
+        // <activity android:name="com.github.pedrovgs.lynx.LynxActivity"/>
+        if (isDebuggable()) {
+            LynxShakeDetector(this).init()
+        }
+    }
+
+    abstract fun isDebuggable(): Boolean
 }
