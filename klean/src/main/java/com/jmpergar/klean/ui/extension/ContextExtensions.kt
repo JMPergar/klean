f3cf71bad3b176a5c90d61043f858a8ff59c1fdd
JMPergar <jm.pereira.g@gmail.com>
2017-03-15 16:17:42 +0100

    Add extension function to get color

--- klean/src/main/java/com/jmpergar/klean/ui/extension/ContextExtensions.kt
+++ klean/src/main/java/com/jmpergar/klean/ui/extension/ContextExtensions.kt
@@ -2,6 +2,7 @@ package com.jmpergar.klean.ui.extension
 
 import android.content.Context
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.widget.Toast
 
 fun Context.showToast(string: String, duration: Int = Toast.LENGTH_SHORT) {
@@ -10,4 +11,8 @@ fun Context.showToast(string: String, duration: Int = Toast.LENGTH_SHORT) {
 
 fun Fragment.showToast(string: String, duration: Int = Toast.LENGTH_SHORT) {
     context.showToast(string, duration)
+}
+
+fun Context.getColorFromResource(colorResource: Int): Int {
+    return ContextCompat.getColor(this, colorResource)
 }
