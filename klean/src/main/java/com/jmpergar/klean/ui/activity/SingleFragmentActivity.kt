315ea23d6e33949f705025ed363701502069892d
JMPergar <jm.pereira.g@gmail.com>
2017-03-16 13:48:02 +0100

    Add avatar support to toolbar in SingleFragmentActivity

--- klean/src/main/java/com/jmpergar/klean/ui/activity/SingleFragmentActivity.kt
+++ klean/src/main/java/com/jmpergar/klean/ui/activity/SingleFragmentActivity.kt
@@ -2,8 +2,11 @@ package com.jmpergar.klean.ui.activity
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.View
 import com.jmpergar.klean.R
 import com.jmpergar.klean.ui.presenter.BasePresenter
+import kotlinx.android.synthetic.main.view_toolbar.*
+import com.jmpergar.klean.ui.extension.loadUrl
 
 abstract class SingleFragmentActivity<out T : BasePresenter<U>, U : BasePresenter.View> : BaseActivity<T, U>() {
 
@@ -17,6 +20,7 @@ abstract class SingleFragmentActivity<out T : BasePresenter<U>, U : BasePresente
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpToolbar()
         if (savedInstanceState == null) {
             supportFragmentManager
                     .beginTransaction()
@@ -25,5 +29,22 @@ abstract class SingleFragmentActivity<out T : BasePresenter<U>, U : BasePresente
         }
     }
 
+    private fun setUpToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
     protected abstract fun initializeFragment(): Fragment
+
+    fun setAvatarClickListener(f: () -> Unit) {
+        toolbarAvatar.setOnClickListener { f() }
+    }
+
+    fun setAvatarImageUrl(url: String) {
+        toolbarAvatar.loadUrl(url)
+    }
+
+    fun setAvatarEnable(enable: Boolean) {
+        toolbarAvatar.visibility = if (enable) { View.VISIBLE } else { View.GONE }
+    }
 }
